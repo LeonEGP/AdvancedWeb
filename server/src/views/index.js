5344e45e009d999fedbe1af6c90dcb74cc0efb50
MasterCraft117 <83456221+MasterCraft117@users.noreply.github.com>
2023-04-20 18:49:12 -0600

    Index add
    Inclusión del archivo index.js

---
+++ server/src/views/index.js
@@ -0,0 +1,10 @@
+<div class = "row" >
+    <div class = "col-md-4 mx-auto" >
+        <div class = "card text-cemter my-3" >
+            <div class = "card-body" >
+                <h3> En home </h3>
+        
+            </div>
+        </div>
+    </div>
+</div>
