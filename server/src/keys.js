52f7828722e7b255d989fa354745303b61b417e3
JuanPabloMontoya271 <juanpablomontoyae@gmail.com>
2023-04-20 18:40:18 -0600

    - Fixed issues with Dockerfile for server - Fixed issues with ENV variable namings - Added .env file compatibility

--- server/src/keys.js
+++ server/src/keys.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 module.exports = {
     database: {
         host: process.env.HOST || 'localhost',
-        user: process.env.USER || 'root',
+        user: process.env.MYSQL_USER || 'root',
         password: process.env.PASSWORD || 'itesm',
         database: process.env.DATABASE || 'test_database'
     }
